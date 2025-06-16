d3c9f540331e78d42448dea743973a6303698fc4
VitorAugusto88 <visilvavidal@gmail.com>
2025-06-16 20:02:11 -0300

    Atualizacao16/06

---
+++ Migrations/20250616225410_Adicionando Pagamento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCC_2025.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoPagamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pagamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FuncionarioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    VendaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TotalPago = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ToTalDeVezes = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamento", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pagamento");
+        }
+    }
+}
---
+++ Models/Pagamento.cs
@@ -0,0 +1,14 @@
+﻿namespace TCC_2025.Models
+{
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public int FuncionarioId { get; set; }
+        public int ClienteId { get; set; }
+        public int VendaId { get; set; }
+        public decimal TotalPago { get; set; }
+        public int ToTalDeVezes { get; set; }
+        public DateTime DataPagamento { get; set; }
+
+    }
+}
